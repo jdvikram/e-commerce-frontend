053c290ba06320b953fdfbbd64f71c09a1f0556b
Vikram Jadhav <er.vikram.jadhav@gmail.com>
2024-02-15 23:52:28 +0530

    add new files

---
+++ src/Constants.js
@@ -0,0 +1,8 @@
+// constants.js
+
+export const API_URL = 'http://127.0.0.1:8000';
+export const LOGIN_ENDPOINT = '/token/';
+export const REFRESH_TOKEN_ENDPOINT = '/token/refresh/';
+export const PRODUCTS_ENDPOINT = '/product/';
+export const USERNAME = 'admin';
+export const PASSWORD = 'admin';
---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+// Footer.js
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>&copy; 2022 E-Shopping. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
